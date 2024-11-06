0290d93d02dfec60a6a90039d57c5bcf9126f408
Prem Sharaan <premjr08@gmail.com>
2024-11-06 12:11:14 +0800

    Update feed.py

--- feed.py
+++ feed.py
@@ -10,7 +10,15 @@ with open('feed.yaml', 'r') as file:
 
 channel_element = xml_tree.SubElement(rss_element, 'channel')
 
+link_prefix = yaml_data['link']
+
 xml_tree.SubElement(channel_element, 'title').text = yaml_data['title']
+xml_tree.SubElement(channel_element, 'format').text = yaml_data['format']
+xml_tree.SubElement(channel_element, 'subtitle').text = yaml_data['subtitle']
+xml_tree.SubElement(channel_element, 'itunes:author').text = yaml_data['author']
+xml_tree.SubElement(channel_element, 'description').text = yaml_data['title']
+xml_tree.SubElement(channel_element, 'itunes:image').text = yaml_data['image']
+xml_tree.SubElement(channel_element, 'language').text = yaml_data['language']
 
 output_tree = xml_tree.ElementTree(rss_element)
 
